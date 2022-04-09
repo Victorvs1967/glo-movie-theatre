4e1ee413c9efb92b6704cd6cd2ff0ca84812ffd4
Victor <victorsmirnov@me.com>
2022-04-09 09:44:54 +0300

    create page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const modalWindow = document.querySelector('.modal'),
+//       buttonModals = document.querySelectorAll('.modal__button'),
+//       body = document.querySelector('body');
+
+// buttonModals.forEach(item => {
+//   item.addEventListener('click', () => {
+//     modalWindow.classList.add('active');
+//     body.classList.add('noscroll');
+//   });
+// });
+
+// modalWindow.addEventListener('click', event => {
+//   const isModal = event.target.closest('.modal__inner');
+//   if (isModal) {
+//     modalWindow.classList.remove('active');
+//     body.classList.remove('noscroll');
+//   }
+// });
